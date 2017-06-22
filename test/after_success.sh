07118e6805b736baa8c0068e4d1b8aff8b787932
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-06-22 21:25:42 +0200

    change directory (/project) before run Maven cmd

--- test/after_success.sh
+++ test/after_success.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd ../project
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn_version=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.5.0:exec)
     echo "Maven version is $mvn_version"
