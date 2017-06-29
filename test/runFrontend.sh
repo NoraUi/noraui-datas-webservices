f29ca7af15ca6793123ae3c4e4cffb397c1a3c62
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-06-30 00:10:22 +0200

    Update runFrontend.sh

--- test/runFrontend.sh
+++ test/runFrontend.sh
@@ -1,15 +1,16 @@
+#!/usr/bin/env bash
 cd $(dirname $0)
 cd ../webclient
 pwd
 ls -l
 npm install
 
-echo ***** START ANGULAR 4 APPLICATION
+echo "***** START ANGULAR 4 APPLICATION"
 ng serve
 
-echo ***** CURL to home url
+echo "***** CURL to home url"
 curl -s http://localhost:4200
 
-echo ***** END
+echo "***** END"
 let ret=0
 exit
