08747a49460fc042229b8d2e99a705f65253709f
sgrillon14 <sgrillon@hotmail.fr>
2018-04-21 00:08:01 +0200

    upgade to V3.x.x

--- project/src/test/java/com/github/noraui/datas/web/services/rest/NoraUiDatasControllerTests.java
+++ project/src/test/java/com/github/noraui/datas/web/services/rest/NoraUiDatasControllerTests.java
@@ -1,27 +1,8 @@
-/* Copyright (c) 2017 NoraUi Organization https://github.com/NoraUi
- * All rights reserved.
+/**
+ * NoraUi is licensed under the license GNU AFFERO GENERAL PUBLIC LICENSE
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <organization> nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * @author Nicolas HALLOUIN
+ * @author Stéphane GRILLON
  */
 package com.github.noraui.datas.web.services.rest;
 
@@ -38,11 +19,11 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.web.client.RestTemplate;
 import org.testng.annotations.Test;
 
-import noraui.data.rest.DataModel;
+import com.github.noraui.data.rest.DataModel;
+import com.google.gson.Gson;
 
 /**
- * @author sgrillon
- *         Basic integration tests for NoraUiDatasControllerController.
+ * Basic integration tests for NoraUiDatasControllerController.
  */
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext
@@ -113,6 +94,7 @@ public class NoraUiDatasControllerTests extends AbstractTestNGSpringContextTests
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
         DataModel dataModel = restTemplate.patchForObject("http://localhost:" + port + "/noraui/api/hello/column/" + column + "/line/" + line, "MY TITLE", DataModel.class);
+        System.out.println(new Gson().toJson(dataModel));
         assertThat(dataModel.getRows().get(line - 1).getColumns().get(column - 1)).isEqualTo("MY TITLE");
     }
 
